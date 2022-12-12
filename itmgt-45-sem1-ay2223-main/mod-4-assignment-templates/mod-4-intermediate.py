2c36904a655421e404135a42ac821bca165946d7
paolojfm <66782274+paolojfm@users.noreply.github.com>
2022-12-12 12:25:23 +0800

    Add files via upload

---
+++ itmgt-45-sem1-ay2223-main/mod-4-assignment-templates/mod-4-intermediate.py
@@ -0,0 +1,170 @@
+'''Module 3: Individual Programming Assignment 1
+
+Thinking Like a Programmer
+
+This assignment covers your intermediate proficiency with Python.
+'''
+
+def shift_letter(letter, shift):
+    '''Shift Letter. 
+    4 points.
+    
+    Shift a letter right by the given number.
+    Wrap the letter around if it reaches the end of the alphabet.
+
+    Examples:
+    shift_letter("A", 0) -> "A"
+    shift_letter("A", 2) -> "C"
+    shift_letter("Z", 1) -> "A"
+    shift_letter("X", 5) -> "C"
+    shift_letter(" ", _) -> " "
+
+    *Note: the single underscore `_` is used to acknowledge the presence
+        of a value without caring about its contents.
+
+    Parameters
+    ----------
+    letter: str
+        a single uppercase English letter, or a space.
+    shift: int
+        the number by which to shift the letter. 
+
+    Returns
+    -------
+    str
+        the letter, shifted appropriately, if a letter.
+        a single space if the original letter was a space.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    alphabet = list(("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"))
+    for x in range(len(alphabet)):
+        if alphabet[x] == letter:
+            index = x 
+        elif alphabet[x] != letter and x == (len(alphabet)) - 1:
+            return letter 
+    final = (index + shift) % 26  
+    return alphabet[final]
+
+def caesar_cipher(message, shift):
+    '''Caesar Cipher. 
+    6 points.
+    
+    Apply a shift number to a string of uppercase English letters and spaces.
+
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and spaces.
+    shift: int
+        the number by which to shift the letters. 
+
+    Returns
+    -------
+    str
+        the message, shifted appropriately.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+    a_list = []
+    for x in list(message):
+        if x == " ":
+            a_list.append(" ")
+        else:
+            index = alphabet.index(x)
+            newindex = index + shift
+            if newindex <= 25:
+                a_list.append(alphabet[newindex])
+            else:
+                while newindex > 25:
+                    newindex = newindex - 26
+                a_list.append(alphabet[newindex])
+    return("".join(a_list))
+
+def shift_by_letter(letter, letter_shift):
+    '''Shift By Letter. 
+    4 points.
+    
+    Shift a letter to the right using the number equivalent of another letter.
+    The shift letter is any letter from A to Z, where A represents 0, B represents 1, 
+        ..., Z represents 25.
+
+    Examples:
+    shift_by_letter("A", "A") -> "A"
+    shift_by_letter("A", "C") -> "C"
+    shift_by_letter("B", "K") -> "L"
+    shift_by_letter(" ", _) -> " "
+
+    Parameters
+    ----------
+    letter: str
+        a single uppercase English letter, or a space.
+    letter_shift: str
+        a single uppercase English letter.
+
+    Returns
+    -------
+    str
+        the letter, shifted appropriately.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+    if letter == " ":
+            return " "
+    else:
+        index = alphabet.index(letter)
+        shift_index = alphabet.index(letter_shift)
+        newindex = index + shift_index
+        if newindex <= 25:
+            return alphabet[newindex]
+        else:
+            while newindex > 25:
+                newindex = newindex - 26
+                return alphabet[newindex]
+
+def vigenere_cipher(message, key):
+    '''Vigenere Cipher. 
+    6 points.
+    
+    Encrypts a message using a keyphrase instead of a static number.
+    Every letter in the message is shifted by the number represented by the 
+        respective letter in the key.
+    Spaces should be ignored.
+
+    Example:
+    vigenere_cipher("A C", "KEY") -> "K A"
+
+    If needed, the keyphrase is extended to match the length of the key.
+        If the key is "KEY" and the message is "LONGTEXT",
+        the key will be extended to be "KEYKEYKE".
+
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and spaces.
+    key: str
+        a string of uppercase English letters. Will never be longer than the message.
+        Will never contain spaces.
+
+    Returns
+    -------
+    str
+        the message, shifted appropriately.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    key = list(key)
+    text = []
+    for x in range(len(message) - len(key)):
+        key.append(key[i % len(key)])
+    
+    for x in range(len(message)):
+        if message[x] == " ":
+            text.append(" ")
+        else:
+            y = (ord(message[x]) + ord(key[x])) % 26
+            y += ord('A')
+            text.append(chr(y))
+    return("".join(text))
